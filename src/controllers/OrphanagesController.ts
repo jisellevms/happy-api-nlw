ea56e5152dc029a594a73192480d5c747c1ad0fd
= <=>
2020-10-26 13:20:05 -0300

    adicionando view e tratando exceções

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,15 +1,19 @@
 import { Request, response, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
+import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
 
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
 
-        return response.json(orphanages);
+        return response.json(orphanageView.renderMany(orphanages));
     },
 
     async show(request: Request, response: Response) {
@@ -18,9 +22,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return response.json(orphanage);
+        return response.json(orphanageView.render(orphanage));
     },
 
     async create(request: Request, response: Response) {
@@ -40,9 +46,9 @@ export default {
 
         const images = requestImages.map(image => {
             return { path: image.filename }
-        })
+        });
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -51,7 +57,9 @@ export default {
             opening_hours,
             open_on_weekends,
             images
-        });
+        }
+
+        const orphanage = orphanagesRepository.create(data);
 
         await orphanagesRepository.save(orphanage)
 
